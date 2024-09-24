b1e1811d384d518270994f93e358ce28f0525ddd
Admin <reteamboss@gmail.com>
2024-09-24 17:10:17 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    println("Hello World!")
+
+    val str = ToPal("доход")
+    MyNum(str).toPal()
+
 }
+
+interface Pal{
+    fun toPal()
+}
+
+class ToPal(val str: String): Pal{
+    override fun toPal() {
+        if (str == str.reversed()){
+            println("Строка палиндром")
+        } else{
+            println("Строка не палиндром")
+        }
+    }
+}
+
+class MyNum(str: Pal):Pal by str
